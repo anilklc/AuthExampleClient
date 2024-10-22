4433a7ecc5934e9bd5b04069ec63b94da3982065
anilklc <hage4950@gmail.com>
2024-10-23 01:10:05 +0300

    AuthorizeMenu edited

---
+++ AuthExampleClient.DTOs/AuthorizeMenu/AssignRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthExampleClient.DTOs.AuthorizeMenu
+{
+    public class AssignRole
+    {
+        public string[] Roles { get; set; }
+        public string Menu { get; set; }
+        public string Code { get; set; }
+    }
+}
--- AuthExampleClient.UI/Areas/Admin/Controllers/AuthorizeMenuController.cs
+++ AuthExampleClient.UI/Areas/Admin/Controllers/AuthorizeMenuController.cs
@@ -1,7 +1,9 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using AuthExampleClient.DTOs.AuthorizeMenu;
+using AuthExampleClient.DTOs.Brand;
 using AuthExampleClient.DTOs.Role;
 using AuthExampleClient.Services.Interfaces;
+using AuthExampleClient.Services.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthExampleClient.UI.Areas.Admin.Controllers
@@ -12,27 +14,48 @@ namespace AuthExampleClient.UI.Areas.Admin.Controllers
     {
         private readonly IReadService<ApplicationService> _readService;
         private readonly IReadService<Role> _roleService;
- 
+        private readonly IWriteService<AssignRole, AssignRole> _writeService;
 
-        public AuthorizeMenuController(IReadService<ApplicationService> readService, IReadService<Role> roleService, INotyfService notyfService) : base(notyfService)
+        public AuthorizeMenuController(IReadService<ApplicationService> readService, IReadService<Role> roleService, INotyfService notyfService, IWriteService<AssignRole, AssignRole> writeService) : base(notyfService)
         {
             _readService = readService;
-            _roleService = roleService; 
+            _roleService = roleService;
+            _writeService = writeService;
         }
 
         [HttpGet("[action]")]
         public async Task<IActionResult> Index()
         {
             var datas = await _readService.GetAllAsync("ApplicationServices/GetAuthorizeDefinitionEndpoints", "");
-            var roles = await _roleService.GetAllAsync("Roles/GetAllRoles", "roles");
-            ViewBag.Roles = roles;
             return View(datas);
         }
 
-        [HttpPost("[action]")]
-        public async Task<IActionResult> AssignRoles(string[] roleIds)
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetRolesForMenu(string menu, string code)
         {
-            return View();
+            var allRoles = await _roleService.GetAllAsync("Roles/GetAllRoles", "roles");
+            menu = menu.Replace(" ", "");
+            var assignedRoles = await _roleService.GetAllAsync($"AuthorizationEndpoints/GetRolesToEndpoint/{menu}/{code}", "roles");
+            var assignedRoleIds = assignedRoles?.Select(r => r.Id).ToArray() ?? new string[] { };
+            var result = new
+            {
+                AllRoles = allRoles,
+                AssignedRoleIds = assignedRoleIds
+            };
+            return Json(result);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AssignRoles([FromBody]AssignRole assignRole)
+        {
+           return await HandleFormAndApiRequestAsync(
+                assignRole,
+                () => _writeService.CreateAsync("AuthorizationEndpoints/AssignRoleEndpoint", assignRole),
+                "Roller başarıyla güncellendi.",
+                "Roller güncellenirken bir hata oluştu."
+            );
+
+            
         }
     }
 }
